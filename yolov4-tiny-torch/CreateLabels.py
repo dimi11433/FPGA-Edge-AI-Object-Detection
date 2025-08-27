2b7cbb5e03647052ca0af028572afaec7ce268c1
dimi11433 <dimitricromarty@gmail.com>
2025-08-27 17:25:17 -0400

    Added labels for pytorch

---
+++ yolov4-tiny-torch/CreateLabels.py
@@ -0,0 +1,32 @@
+import os
+
+input_file = "dataset/test/_annotations.txt"
+output_dir = "dataset/labels"
+
+os.makedirs(output_dir, exist_ok=True)
+
+
+with open(input_file, "r") as file_object:
+    for line in file_object:
+        parts = line.strip().split(" ")
+        if len(parts) < 2:
+            continue
+        image_name = parts[0]
+        farts = parts[1].split(",")
+        coords = " ".join(farts)
+        
+        # create a label file with same name but .txt
+        label_filename = os.path.splitext(image_name)[0] + ".txt"
+        label_path = os.path.join(output_dir, label_filename)
+
+        with open(label_path, "a") as f:  # "a" in case multiple objects per image
+            f.write(coords + "\n")
+            
+    
+    
+
+
+
+            
+            
+        
